62b8952703782508136a25710f0fe0a138e385e1
Miikhaaeel <fadowomichael1@gmail.com>
2021-08-04 14:41:59 +0100

    created question interface

--- lib/screens/quiz/quiz_screen.dart
+++ lib/screens/quiz/quiz_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_fcb/constants.dart';
+import 'package:quiz_fcb/models/Questions.dart';
 import 'package:quiz_fcb/screens/components/progress_bar.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
@@ -34,6 +35,54 @@ class QuizScreen extends StatelessWidget {
               child: Column(
                 children: [
                   ProgressBar(),
+                  SizedBox(height: kDefaultPadding),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Question 1',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          ?.copyWith(color: kSecondaryColor),
+                      children: [
+                        TextSpan(
+                          text: '/10',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(color: kSecondaryColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    thickness: 1.5,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(kDefaultPadding),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          sample_data[0]['question'],
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(color: kBlackColor),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: kDefaultPadding),
+                          padding: EdgeInsets.all(kDefaultPadding),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: kGrayColor),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
@@ -43,4 +92,3 @@ class QuizScreen extends StatelessWidget {
     );
   }
 }
-
